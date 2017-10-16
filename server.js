0f108dd2f100128227757074420874902d3a8bfe
brian <brianf_lp@hotmail.com>
2017-10-16 16:32:51 -0300

    Agrego primera implementacion de servidor TCP

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const Sequelize = require('sequelize');
 var formidable = require("formidable"),
     util = require('util');
 var models  = require('./models');
+var net = require('net');
+
+var tcpGuests = [];
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
@@ -12,12 +16,12 @@ app.get('/', function(req, res){
             
     models.Mensaje.findAll({limit: 5, order: [['updatedAt', 'DESC']]}).then(function(mensajes) {
         models.Conf.findAll({limit:1,order: [['updatedAt', 'DESC']] }).then(function(conf) {
-        res.render('index', {
-        title: 'Pet Center',
-        mensajes: mensajes,
-        conf: conf
+            res.render('index', {
+                title: 'Pet Center',
+                mensajes: mensajes,
+                conf: conf
+            })
         })
-    })
     })
             //res.sendFile(__dirname+'/public/index.html');
         
@@ -36,8 +40,12 @@ function procesarForm(req,res){
                         console.log(msj.get({plain: true}))
                     }
                 )
+                //Transmitir los cambios de configuracion a la CIAA
+                for (g in tcpGuests){
+                    tcpGuests[g].write(conf.racion + ' | ' + conf.hora_actual +' | '+ conf.hora_alarma);
+                }  
             }
-            //ACÁ VA CÓDIGO PARA TRANSMITIR MENSAJE A LA CIAA
+            
         )
     });
     models.Mensaje.findAll({limit: 5, order: [['updatedAt', 'DESC']]}).then(function(mensajes) {
@@ -48,9 +56,29 @@ function procesarForm(req,res){
                 conf: conf
                 })
             })
-            })
+    })
 }
-
+var server = net.createServer(function(socket) {
+    socket.setEncoding('utf8');
+    tcpGuests.push(socket);
+    // Cuando se conecta la CIAA al servidor baja la ultima configuracion, por si hubo cambios. Esto no está funcionando por ahora.
+    models.Mensaje.findAll({limit:1,order: [['updatedAt', 'DESC']] }).then(function(msj) {
+            var mssj = msj.contenido;
+            console.log(mssj);
+            socket.write("Hola");
+	        socket.pipe(socket);
+        });
+    socket.on('data',function(data){
+        //La CIAA envia un mensaje y el servidor lo guarda para mostrarlo en la tabla de eventos.
+        console.log(data);
+        models.Mensaje.create({contenido: data}).then(
+            msj =>{
+                console.log(msj.get({plain: true}))
+            }
+        )
+        socket.write('dijiste'+data);
+    });
+}).listen(1337, '127.0.0.1'); 
 http.listen(8090, function(){
     console.log('server http en 8090');
 });
