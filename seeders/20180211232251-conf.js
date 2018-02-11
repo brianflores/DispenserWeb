cdc7b8f5d0d8d7513d3188adb408eae2fceaf2b7
brian <brianf_lp@hotmail.com>
2018-02-11 20:29:03 -0300

    Agrego seeder para configuracion inicial

---
+++ seeders/20180211232251-conf.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Confs',[
+      {hora_alarma: "11:00" , racion: "pequenio", hora_actual: "11:00", createdAt: new Date(), updatedAt: new Date()}
+      ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
